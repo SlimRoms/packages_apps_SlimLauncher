5906e04e6efdf5dbaa8170f7fda018472e38276d
Griffin Millender <griffinn.millender@gmail.com>
2014-09-12 17:15:09 -0500

    Cleanup settings keys
    Change-Id: I2280a846de8267db59cb251ef53f233ec9f8232e

--- src/com/slim/slimlauncher/settings/SettingsKeys.java
+++ src/com/slim/slimlauncher/settings/SettingsKeys.java
@@ -20,16 +20,4 @@ public interface SettingsKeys {
     public static final String PINCH_GESTURE_ACTION = "pinch_gesture_action";
     public static final String SPREAD_GESTURE_ACTION = "spread_gesture_action";
     public static final String DOUBLE_TAP_GESTURE_ACTION = "double_tap_gesture_action";
-
-    // Notification Badges
-    public static final String KEY_ENABLE_BADGES = "enable_badges";
-    public static final String KEY_BADGE_POSITION = "badge_position";
-    public static final String KEY_BADGE_FRAME_SIZE = "badge_frame_size";
-    public static final String KEY_BADGE_TEXT_SIZE = "badge_text_size";
-    public static final String KEY_BADGE_CORNER_RADIUS = "badge_corner_radius";
-    public static final String KEY_BADGE_SIDE_PADDING = "badge_side_padding";
-    public static final String KEY_BADGE_TOP_PADDING = "badge_top_padding";
-    public static final String KEY_BADGE_BACKGROUND_COLOR = "badge_background_color";
-    public static final String KEY_BADGE_TEXT_COLOR = "badge_text_color";
-    public static final String KEY_BADGE_FRAME_COLOR = "badge_frame_color";
 }
