758e5eff6ebebdc3414779b39b6db11d9cfa94eb
Griffin Millender <griffinn.millender@gmail.com>
2014-10-13 14:25:24 -0500

    Fix preferences and add copyright to settings screen
    Change-Id: I1f71522263142352340dc894d3885e731611fad3

--- src/com/slim/slimlauncher/settings/SettingsActivity.java
+++ src/com/slim/slimlauncher/settings/SettingsActivity.java
@@ -11,5 +11,20 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     public void onBuildHeaders(List<Header> target) {
         loadHeadersFromResource(R.xml.preferences, target);
+        updateHeaders(target);
+    }
+
+    private void updateHeaders(List<Header> headers) {
+        for (Header header : headers) {
+            if (header.id == R.id.slim_application_version) {
+                header.title = getString(R.string.slim_application_name) + " "
+                        + getString(R.string.slim_application_version);
+            }
+        }
+    }
+
+    @Override
+    protected boolean isValidFragment(String fragmentName) {
+        return true;
     }
 }
