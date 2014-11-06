5a4287af1c2ec47900faf8b5a0788ff2ef5b128f
Sunny Goyal <sunnygoyal@google.com>
2014-11-06 10:12:54 -0800

    Using the default search widget in Launcher3
    > Removing all logic related to search and voice overlays
    from Launcher3
    > Using the widget provided by global search provider on the
    homescreen
    > Removing VoiceButtonProxy, as it is not being used anymore
    
    Change-Id: Ie8b09b44f7213c8fa11bce685914442e4884295d

--- src/com/slim/slimlauncher/Workspace.java
+++ src/com/slim/slimlauncher/Workspace.java
@@ -1431,13 +1431,11 @@ public class Workspace extends SmoothPagedView
             if (mCustomContentCallbacks != null) {
                 mCustomContentCallbacks.onShow(false);
                 mCustomContentShowTime = System.currentTimeMillis();
-                mLauncher.updateVoiceButtonProxyVisible(false);
             }
         } else if (hasCustomContent() && getNextPage() != 0 && mCustomContentShowing) {
             mCustomContentShowing = false;
             if (mCustomContentCallbacks != null) {
                 mCustomContentCallbacks.onHide();
-                mLauncher.updateVoiceButtonProxyVisible(false);
             }
         }
     }
@@ -2352,16 +2350,6 @@ public class Workspace extends SmoothPagedView
         return -offsetFromTopEdge + mInsets.top + offsetToCenterInOverview;
     }
 
-    boolean shouldVoiceButtonProxyBeVisible() {
-        if (isOnOrMovingToCustomContent()) {
-            return false;
-        }
-        if (mState != State.NORMAL) {
-            return false;
-        }
-        return true;
-    }
-
     public void updateInteractionForState() {
         if (mState != State.NORMAL) {
             mLauncher.onInteractionBegin();
@@ -2630,7 +2618,6 @@ public class Workspace extends SmoothPagedView
             setScaleY(mNewScale);
             setTranslationY(finalWorkspaceTranslationY);
         }
-        mLauncher.updateVoiceButtonProxyVisible(false);
 
         if (stateIsNormal) {
             animateBackgroundGradient(0f, animated);
