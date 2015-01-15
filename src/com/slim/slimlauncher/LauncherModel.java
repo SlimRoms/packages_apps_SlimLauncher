LineageOS/android_packages_apps_Trebuchet

Launcher: show unread notify info number at APP icon on Launcher
Show APP's unread info number at Launcher:

  - Register a receiver to receive the UNREAD_CHANGED event to update the APP icon

  - When the unread number changed, recreate the icon bitmap of icon cache

Change-Id: I7dd0fb2c29959f1584682965fb1476e3f3c77739

src/com/android/launcher3/LauncherModel.java
@@ -178,6 +178,9 @@ public class LauncherModel extends BroadcastReceiver
 
     // </ only access in worker thread >
 
+    public static final String ACTION_UNREAD_CHANGED =
+            "com.android.launcher.action.UNREAD_CHANGED";
+
     private IconCache mIconCache;
 
     protected int mPreviousConfigMcc;
@@ -213,6 +216,58 @@ public class LauncherModel extends BroadcastReceiver
         public void onPageBoundSynchronously(int page);
         public void dumpLogsToLocalData();
     }
+
+    private HashMap<ComponentName, UnreadInfo> unreadChangedMap =
+            new HashMap<ComponentName, LauncherModel.UnreadInfo>();
+
+    private class UnreadInfo {
+        ComponentName mComponentName;
+        int mUnreadNum;
+
+        public UnreadInfo(ComponentName componentName, int unreadNum) {
+            mComponentName = componentName;
+            mUnreadNum = unreadNum;
+        }
+    }
+
+    private class UnreadNumberChangeTask implements Runnable {
+        public void run() {
+            ArrayList<UnreadInfo> unreadInfos = new ArrayList<LauncherModel.UnreadInfo>();
+            synchronized (unreadChangedMap) {
+                unreadInfos.addAll(unreadChangedMap.values());
+                unreadChangedMap.clear();
+            }
+
+            Context context = mApp.getContext();
+            final Callbacks callbacks = mCallbacks != null ? mCallbacks.get() : null;
+            if (callbacks == null) {
+                Log.w(TAG, "Nobody to tell about the new app.  Launcher is probably loading.");
+                return;
+            }
+
+            final ArrayList<AppInfo> unreadChangeFinal = new ArrayList<AppInfo>();
+            for (UnreadInfo uInfo : unreadInfos) {
+                AppInfo info = mBgAllAppsList.unreadNumbersChanged(context,
+                        uInfo.mComponentName, uInfo.mUnreadNum);
+                if (info != null) {
+                    unreadChangeFinal.add(info);
+                }
+            }
+
+            if (unreadChangeFinal.isEmpty()) return;
+
+            mHandler.post(new Runnable() {
+                public void run() {
+                    Callbacks cb = mCallbacks != null ? mCallbacks.get() : null;
+                    if (callbacks == cb && cb != null) {
+                        callbacks.bindAppsUpdated(unreadChangeFinal);
+                    }
+                }
+            });
+        }
+    }
+
+    private UnreadNumberChangeTask mUnreadUpdateTask = new UnreadNumberChangeTask();
 
     public interface ItemInfoFilter {
         public boolean filterItem(ItemInfo parent, ItemInfo info, ComponentName cn);
@@ -1334,6 +1389,16 @@ public class LauncherModel extends BroadcastReceiver
                     callbacks.bindSearchablesChanged();
                 }
             }
+        } else if (ACTION_UNREAD_CHANGED.equals(action)) {
+            ComponentName componentName = intent.getParcelableExtra("component_name");
+            int unreadNum = intent.getIntExtra("unread_number", 0);
+
+            if (componentName == null) return;
+            synchronized (unreadChangedMap) {
+                unreadChangedMap.put(componentName, new UnreadInfo(componentName, unreadNum));
+            }
+            sWorker.removeCallbacks(mUnreadUpdateTask);
+            sWorker.post(mUnreadUpdateTask);
         }
     }
 

SlimRoms/packages_apps_SlimLauncher

src/com/slim/slimlauncher/LauncherModel.java
@@ -174,6 +174,9 @@ public class LauncherModel extends BroadcastReceiver
 
     // </ only access in worker thread >
 
+    public static final String ACTION_UNREAD_CHANGED =
+            "com.slim.slimlauncher.action.UNREAD_CHANGED";
+
     private IconCache mIconCache;
 
     protected int mPreviousConfigMcc;
@@ -209,6 +212,58 @@ public class LauncherModel extends BroadcastReceiver
         public void onPageBoundSynchronously(int page);
         public void dumpLogsToLocalData();
     }
+
+    private HashMap<ComponentName, UnreadInfo> unreadChangedMap =
+            new HashMap<ComponentName, LauncherModel.UnreadInfo>();
+
+    private class UnreadInfo {
+        ComponentName mComponentName;
+        int mUnreadNum;
+
+        public UnreadInfo(ComponentName componentName, int unreadNum) {
+            mComponentName = componentName;
+            mUnreadNum = unreadNum;
+        }
+    }
+
+    private class UnreadNumberChangeTask implements Runnable {
+        public void run() {
+            ArrayList<UnreadInfo> unreadInfos = new ArrayList<LauncherModel.UnreadInfo>();
+            synchronized (unreadChangedMap) {
+                unreadInfos.addAll(unreadChangedMap.values());
+                unreadChangedMap.clear();
+            }
+
+            Context context = mApp.getContext();
+            final Callbacks callbacks = mCallbacks != null ? mCallbacks.get() : null;
+            if (callbacks == null) {
+                Log.w(TAG, "Nobody to tell about the new app.  Launcher is probably loading.");
+                return;
+            }
+
+            final ArrayList<AppInfo> unreadChangeFinal = new ArrayList<AppInfo>();
+            for (UnreadInfo uInfo : unreadInfos) {
+                AppInfo info = mBgAllAppsList.unreadNumbersChanged(context,
+                        uInfo.mComponentName, uInfo.mUnreadNum);
+                if (info != null) {
+                    unreadChangeFinal.add(info);
+                }
+            }
+
+            if (unreadChangeFinal.isEmpty()) return;
+
+            mHandler.post(new Runnable() {
+                public void run() {
+                    Callbacks cb = mCallbacks != null ? mCallbacks.get() : null;
+                    if (callbacks == cb && cb != null) {
+                        callbacks.bindAppsUpdated(unreadChangeFinal);
+                    }
+                }
+            });
+        }
+    }
+
+    private UnreadNumberChangeTask mUnreadUpdateTask = new UnreadNumberChangeTask();
 
     public interface ItemInfoFilter {
         public boolean filterItem(ItemInfo parent, ItemInfo info, ComponentName cn);
@@ -1328,6 +1383,18 @@ public class LauncherModel extends BroadcastReceiver
                     callbacks.bindSearchablesChanged();
                 }
             }
+        } else if (ACTION_UNREAD_CHANGED.equals(action)) {
+            String packageName = intent.getStringExtra("packageName");
+            ComponentName componentName = mBgAllAppsList
+                    .getComponentNameForPackageName(packageName);
+            int unreadNum = intent.getIntExtra("count", 0);
+
+            if (componentName == null) return;
+            synchronized (unreadChangedMap) {
+                unreadChangedMap.put(componentName, new UnreadInfo(componentName, unreadNum));
+            }
+            sWorker.removeCallbacks(mUnreadUpdateTask);
+            sWorker.post(mUnreadUpdateTask);
         }
     }
 