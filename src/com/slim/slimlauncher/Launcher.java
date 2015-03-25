721b7f42b2787e7e31618fe9c0ee024a6b09e6b5
Linus Lee <llee@cyngn.com>
2015-03-24 17:15:10 -0700

    App drawer: add animations and letters to make things pretty
    Change-Id: Iaf6718893e09df715b4524b864be9a7c22addc59
    (cherry picked from commit e002b691b3233629598c77ef50f1ef91eb1877f0)

--- src/com/slim/slimlauncher/Launcher.java
+++ src/com/slim/slimlauncher/Launcher.java
@@ -20,6 +20,7 @@ package com.slim.slimlauncher;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.animation.TimeInterpolator;
@@ -56,6 +57,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -1600,6 +1602,17 @@ public class Launcher extends Activity
             }
             mAppDrawer.setHasFixedSize(true);
             mAppDrawer.setAdapter(mAppDrawerAdapter);
+            mAppDrawer.setOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                    mAppDrawerAdapter.onScrollStateChanged(recyclerView, newState);
+                }
+
+                @Override
+                public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                    mAppDrawerAdapter.onScrolled(recyclerView, dx, dy);
+                }
+            });
             initializeScrubber();
         }
     }
@@ -3715,6 +3728,14 @@ public class Launcher extends Activity
 
             mStateAnimation.addListener(new AnimatorListenerAdapter() {
                 @Override
+                public void onAnimationStart(Animator animation) {
+                    if (drawer && mAppsCustomizeContent.getContentType()
+                            == AppsCustomizePagedView.ContentType.Applications) {
+                        updateStatusBarColor(res.getColor(R.color.app_drawer_drag_background));
+                    }
+                }
+
+                @Override
                 public void onAnimationEnd(Animator animation) {
                     dispatchOnLauncherTransitionEnd(fromView, animated, false);
                     dispatchOnLauncherTransitionEnd(toView, animated, false);
@@ -3783,6 +3804,11 @@ public class Launcher extends Activity
             toView.setScaleY(1.0f);
             toView.setVisibility(View.VISIBLE);
             toView.bringToFront();
+            if (drawer && mAppsCustomizeContent.getContentType()
+                    == AppsCustomizePagedView.ContentType.Applications) {
+                updateStatusBarColor(res.getColor(R.color.app_drawer_drag_background));
+                toView.setBackgroundColor(res.getColor(R.color.app_drawer_background));
+            }
 
             if (!springLoaded && !LauncherAppState.getInstance().isScreenLarge()) {
                 // Hide the search bar
@@ -3893,6 +3919,7 @@ public class Launcher extends Activity
                     content.setPageBackgroundsVisible(false);
                 } else {
                     fromView.setBackgroundColor(Color.TRANSPARENT);
+                    updateStatusBarColor(Color.TRANSPARENT);
                 }
 
                 revealView.setTranslationY(0);
@@ -4074,6 +4101,11 @@ public class Launcher extends Activity
             fromView.post(startAnimRunnable);
         } else {
             fromView.setVisibility(View.GONE);
+            if (drawer && mAppsCustomizeContent.getContentType()
+                    == AppsCustomizePagedView.ContentType.Applications) {
+                fromView.setBackgroundColor(Color.TRANSPARENT);
+                updateStatusBarColor(Color.TRANSPARENT, 0);
+            }
             dispatchOnLauncherTransitionPrepare(fromView, animated, true);
             dispatchOnLauncherTransitionStart(fromView, animated, true);
             dispatchOnLauncherTransitionEnd(fromView, animated, true);
@@ -5521,6 +5553,19 @@ public class Launcher extends Activity
 
     void showSearch() {
         if (mSearchDropTargetBar != null) mSearchDropTargetBar.showSearchBar(false);
+    }
+
+    private void updateStatusBarColor(int color) {
+        updateStatusBarColor(color, 300);
+    }
+
+    private void updateStatusBarColor(int color, int duration) {
+        final Window window = getWindow();
+        ObjectAnimator animator = ObjectAnimator.ofInt(window,
+                "statusBarColor", window.getStatusBarColor(), color);
+        animator.setEvaluator(new ArgbEvaluator());
+        animator.setDuration(duration);
+        animator.start();
     }
 
     public ItemInfo createAppDragInfo(Intent appLaunchIntent) {
