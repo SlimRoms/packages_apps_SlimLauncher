f049944b8f9e8dda1a5a27208c7dcda068c5ab1e
scoute-dich <baumann.juergen@posteo.de>
2017-05-27 19:47:08 +0200

    SlimLauncher: fix search bar
    Change-Id: Ib427f0d5034965b2dc68d3cb08f144bd4c079fbc

--- src/com/android/launcher3/allapps/AllAppsContainerView.java
+++ src/com/android/launcher3/allapps/AllAppsContainerView.java
@@ -58,6 +58,9 @@ import com.android.launcher3.shortcuts.DeepShortcutsContainer;
 import com.android.launcher3.userevent.nano.LauncherLogProto.Target;
 import com.android.launcher3.util.ComponentKey;
 
+import org.slim.launcher.SlimLauncher;
+import org.slim.launcher.settings.SettingsProvider;
+
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
 import java.util.ArrayList;
@@ -311,6 +314,15 @@ public class AllAppsContainerView extends BaseContainerView implements DragSourc
 
     public void setSearchBarContainerViewVisibility(int visibility) {
         mSearchContainer.setVisibility(visibility);
+
+        MarginLayoutParams mlp = (MarginLayoutParams) mAppsRecyclerView.getLayoutParams();
+        Rect insets = mLauncher.getDragLayer().getInsets();
+        getContentView().setPadding(0, 0, 0, 0);
+        DeviceProfile grid = mLauncher.getDeviceProfile();
+        mlp.topMargin = insets.top + (visibility == VISIBLE ? grid.hotseatCellHeightPx : 0);
+        mAppsRecyclerView.setLayoutParams(mlp);
+        mApps.updateAdapterItems();
+        mAdapter.notifyDataSetChanged();
         //updatePaddingsAndMargins();
     }
 
@@ -488,6 +500,8 @@ public class AllAppsContainerView extends BaseContainerView implements DragSourc
 
                 mlp.topMargin = height;
                 mAppsRecyclerView.setLayoutParams(mlp);
+                SlimLauncher.getInstance().preferenceChanged(
+                SettingsProvider.KEY_DRAWER_SEARCH_ENABLED);
 
                 mSearchContainer.setPadding(
                         mSearchContainer.getPaddingLeft(),
